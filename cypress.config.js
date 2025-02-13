6e4ab911840e4c8f81a60ed67768f10ade2ca0e6
DevHyagooc <hyagooc.dev@gmail.com>
2025-02-13 18:28:05 -0300

    :wrench: Alteração na url base

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.riotgames.com/pt-br', //DEFINA A URL BASE QUE A APLICAÇÃO DE TESTES VAI ACESSAR
+    baseUrl: 'https://www.nuuvem.com/br-pt', //DEFINA A URL BASE QUE A APLICAÇÃO DE TESTES VAI ACESSAR
     experimentalRunAllSpecs: true,
     defaultCommandTimeout: 2000,
   },
