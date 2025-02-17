e944ce6237fe93d489f6d35af81f6221c2ca7a61
DevHyagooc <hyagooc.dev@gmail.com>
2025-02-17 12:33:20 -0300

    ⚙ Alteração no viewport e no timeout

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.nuuvem.com/br-pt', //DEFINA A URL BASE QUE A APLICAÇÃO DE TESTES VAI ACESSAR
     experimentalRunAllSpecs: true,
-    defaultCommandTimeout: 2000,
+    defaultCommandTimeout: 4000,
   },
   fixturesFolder: false,
   video: false,
-  viewportWidth: 1600,
-  viewportHeight: 900,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
 })
