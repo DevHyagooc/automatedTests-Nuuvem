eead2b07521838aa69a169d95d7299270b1e3d6a
DevHyagooc <hyagooc.dev@gmail.com>
2025-02-17 16:58:44 -0300

    :test_tube: Adição de comandos para teste de linguagens e Produtos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,14 +25,14 @@ Cypress.Commands.add('gui_acessarRedeSocial', (title) => {
   cy.get(`a[title="${title}"]`)
     .should('exist')
     .invoke('removeAttr', 'target')
-    .click()    
+    .click()
 });
 
 Cypress.Commands.add('gui_acessarPaginaFooter', (title) => {
   cy.get(`nav.footer__list`)
     .contains(title)
     .should('exist')
-    .click()    
+    .click()
 });
 
 Cypress.Commands.add('gui_acessarNovaAbaFooter', (title) => {
@@ -40,5 +40,49 @@ Cypress.Commands.add('gui_acessarNovaAbaFooter', (title) => {
     .contains(title)
     .should('exist')
     .invoke('removeAttr', 'target')
-    .click()    
+    .click()
+});
+
+Cypress.Commands.add('gui_buscarProduto', (produto) => {
+  cy.get('.header-search--large > .header-search-wrapper--retracted input[type=search][placeholder=Buscar]')
+    .should('exist')
+    .type(`${produto}{enter}`)
+});
+
+Cypress.Commands.add('gui_adicionarCarrinho', () => {
+  cy.get('.product-section.product-section--buy.hide-down-m')
+    .should('be.visible')
+    .within(() => {
+      cy.wait(2000)
+      cy.get('button[title="Comprar"]')
+        .should('be.visible')
+        .click()
+    })
+});
+
+Cypress.Commands.add('gui_removerCarrinho', () => {
+  cy.get('.btn--lg')
+    .should('be.visible')
+    .click()
+  cy.get('.header-cart-nav--foot > .btn')
+    .should('be.visible')
+    .click()
+  cy.origin('https://secure.nuuvem.com', () => {
+    cy.get('.product-card--wrapper-col')
+      .contains('Alan Wake 2')
+      .should('exist')
+    cy.get('.icon.icon-delete.icon-normal')
+      .click()
+  })
+
+});
+
+Cypress.Commands.add('gui_mudarLinguagem', (language) => {
+  cy.get('.store-icon.store-icon-forum')
+    .should('exist')
+    .click()
+  cy.get('li')
+    .contains(language)
+    .should('exist')
+    .click()
 });
