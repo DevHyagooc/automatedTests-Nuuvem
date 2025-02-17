ddefac30db436b540baa5465f8b64c8f8c4e448e
DevHyagooc <hyagooc.dev@gmail.com>
2025-02-17 13:21:04 -0300

    � Finalização dos comandos para acessar outras páginas do sistemas

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,44 @@
-Cypress.Commands.add('gui_quemSomos', () => {
- 
+Cypress.Commands.add('gui_acessarPaginaHeader', (headerMenu, nameLi) => {
+  cy.get(headerMenu)
+    .should('exist')
+    .trigger('mouseover')
+
+  cy.get('li')
+    .contains(nameLi)
+    .should('be.visible')
+    .click()
+});
+
+Cypress.Commands.add('gui_acessarNovaAbaHeader', (headerMenu, nameLi) => {
+  cy.get(headerMenu)
+    .should('exist')
+    .trigger('mouseover')
+
+  cy.get('li')
+    .contains(nameLi)
+    .should('be.visible')
+    .invoke('removeAttr', 'target')
+    .click()
+});
+
+Cypress.Commands.add('gui_acessarRedeSocial', (title) => {
+  cy.get(`a[title="${title}"]`)
+    .should('exist')
+    .invoke('removeAttr', 'target')
+    .click()    
+});
+
+Cypress.Commands.add('gui_acessarPaginaFooter', (title) => {
+  cy.get(`nav.footer__list`)
+    .contains(title)
+    .should('exist')
+    .click()    
+});
+
+Cypress.Commands.add('gui_acessarNovaAbaFooter', (title) => {
+  cy.get(`nav.footer__list`)
+    .contains(title)
+    .should('exist')
+    .invoke('removeAttr', 'target')
+    .click()    
 });
